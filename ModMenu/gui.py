273802601f308383109fa747009b73fba71db2a7
n0 <74681675+LouisTheXIV@users.noreply.github.com>
2021-12-08 14:19:24 +0200

    Update gui.py

--- ModMenu/gui.py
+++ ModMenu/gui.py
@@ -2,7 +2,9 @@ from tkinter import *
 from pymem import *
 from pymem.process import *
 from settings import *
-
+import keyboard as kb
+from threading import Thread
+from time import sleep
 
 mem = Pymem("ac_client.exe")
 module = module_from_name(mem.process_handle, "ac_client.exe").lpBaseOfDll
@@ -16,13 +18,12 @@ def getPointerAddr(base, offsets):
     addr = addr + offsets[-1]
     return addr
 
-
 class ModMenu():
     def __init__(self, window_title, width, height):
         self.win = Tk()
-        self.win.title(window_title)
-        self.win.geometry(f"{width}x{height}")
-        self.win.wm_overrideredirect(True)
+        x, y = self.center(width, height)
+        self.win.geometry(f"{width}x{height}+{x}+{y}")
+        self.win.overrideredirect(True)
         self.win.wm_attributes("-topmost", 1)
         self.win.wm_attributes("-alpha", 0.7)
         self.win.configure(background=BG)
@@ -30,16 +31,46 @@ class ModMenu():
         self.title_label = Label(self.win, text=window_title, font=('Arial',12), bg=BG, fg=FG)
         self.title_label.pack()
 
-        self.ammo_btn = Button(self.win, text="Ammo Hack", font=('Arial',12), bg=BG, fg=FG, command=self.ammo_hack)
+        self.ammo_btn = Button(self.win, text="Ammo Hack", font=('Arial',14), bg=BG, fg=FG, command=self.ammo_hack)
         self.ammo_btn.pack()
 
-        self.close_btn = Button(self.win, text="Close", font=('Arial',12), bg=BG, fg=FG, command=self.win.destroy)
-        self.close_btn.pack()
+        self.exit_btn = Button(self.win, text="Exit", font=('Arial',14), bg=BG, fg=FG, command=self.win.destroy)
+        self.exit_btn.place(x=160, y=150)
 
     def ammo_hack(self):
         mem.write_int(getPointerAddr(module + 0x0011E20C, ammo_offsets), 1000)
 
+    def center(self, width, height):
+        swidth = self.win.winfo_screenwidth()
+        sheight = self.win.winfo_screenheight()
+        x = (swidth/2) - (width/2)
+        y = (sheight/2) - (height/2)
+        return int(x), int(y)
 
 
-modmenu = ModMenu("Assault Cube Modmenu", 350, 200)
+
+def keybinds(modmenu):
+    isopen = True
+    while True:
+        if kb.is_pressed(OPEN):
+            if isopen == True:
+                modmenu.win.withdraw()
+                isopen = False
+            else:
+                modmenu.win.deiconify()
+                isopen = True
+                modmenu.win.focus_force()
+            sleep(0.5)
+
+
+
+
+
+
+modmenu = ModMenu("Assault Cube Hack", 350, 200)
+
+keybinds_thread = Thread(target=keybinds, args=(modmenu,))
+keybinds_thread.setDaemon(True)
+keybinds_thread.start()
+
 modmenu.win.mainloop()
